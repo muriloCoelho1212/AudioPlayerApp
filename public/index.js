7ef5b43dabdb561b69d1d2d612c2e139e869a4a3
Valkorz <pivarcivittorio@gmail.com>
2024-11-04 20:40:36 -0300

    Fixed categories
    Co-authored-by: Luis Medeiros <luisenbon@users.noreply.github.com>
    Co-authored-by: Murilo Coelho <murilodev.coelho@gmail.com>

--- public/index.js
+++ public/index.js
@@ -1,28 +1,29 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const ctgBox = document.getElementById('categories');
     console.log(`got ctg: ${ctgBox}`);
 
     fetch('content/data/audiodata.json')
-    .then(response => response.json())
-    .then(data => {
-        const categories = []
-        data.map((entry) =>{
-            if(!categories.includes(entry.category)){
-                categories.push(entry.category);
-            }
+        .then(response => response.json())
+        .then(data => {
+            const categories = []
+            data.map((entry) => {
+                if (!categories.includes(entry.category)) {
+                    categories.push(entry.category);
+                }
+            })
+
+            categories.forEach(category => {
+                const ctg = document.createElement('button');
+                ctg.innerHTML = `<h5>${category.toLowerCase()}</h5>`;
+                ctg.style.width = '100px';
+                ctg.style.borderRadius = '10%';
+                ctg.classList.add('category-button', 'btn', 'btn-dark', 'category-button');
+                ctg.onclick = () => {
+                    window.location.href = `/pages/songlist.html?category=${category.toLowerCase()}`;
+                };
+                ctgBox.appendChild(ctg);
+            });
+
         })
-
-        console.log(`mapped data: ${categories}`);
-
-        categories.forEach(category => {
-            const ctg = document.createElement('button');
-            ctg.innerHTML = `<h5>${category.toLowerCase()}</h5>`;
-            ctg.style.width = '100px'
-            ctg.onclick = () => {
-                window.location.href = `/pages/songlist.html?category=${category.toLowerCase()}`;
-            };
-            ctgBox.appendChild(ctg);
-        });
-    })
-    .catch(error => console.log(error))
+        .catch(error => console.log(error))
 });
--- public/pages/songlist.js
+++ public/pages/songlist.js
@@ -1,24 +1,36 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const songlist = document.getElementById('songlist');
     songlist.innerHTML = '';
     const urlParams = new URLSearchParams(window.location.search);
     const category = urlParams.get('category');
 
     fetch('../content/data/audiodata.json')
-    .then(response => response.json())
-    .then(data => {
-        data.map(entry => {
-            if(entry.category.toLowerCase() == category.toLowerCase()){
-                const songbtn = document.createElement('button');
-                const img = document.createElement('img');
-                songbtn.innerHTML = `${entry.title} || ${entry.duration}`;
-                img.src = entry.cover;
-                img.width = 200;
-                songlist.appendChild(songbtn);
-                songlist.appendChild(img);
-            }
+        .then(response => response.json())
+        .then(data => {
+            data.map(entry => {
+                if (entry.category.toLowerCase() == category.toLowerCase()) {
+                    const div = document.createElement('div');
+                    div.style.display = 'flex';
+                    div.style.flexDirection = 'column';
+                    div.style.alignItems = 'center';
+                    div.style.marginBottom = '20px';
+
+                    const songbtn = document.createElement('button');
+                    const img = document.createElement('img');
+                    songbtn.innerHTML = `${entry.title} || ${entry.duration}`;
+                    img.src = entry.cover;
+                    img.style.maxWidth = '100%';
+                    img.style.height = 'auto';
+                    img.style.width = '200px';
+                    img.style.maxHeight = '300px';
+
+
+                    songlist.appendChild(div);
+                    div.appendChild(songbtn);
+                    div.appendChild(img);
+                }
+            })
         })
-    })
-    .catch(error => console.log(error));
+        .catch(error => console.log(error));
 
 });
