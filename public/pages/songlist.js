04c1c84d8c2e62cfa915b6df81257a558ee54222
luisenbon <luismedeirosfi@gmail.com>
2024-11-18 00:45:50 -0300

    Added all 35 musics, and adjusted json

--- public/pages/songlist.js
+++ public/pages/songlist.js
@@ -4,20 +4,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const category = urlParams.get('category');
 
-    const ctgBox = document.getElementById('categories');
-    window.fetchCategories(ctgBox); //nao
-
     fetch('../content/data/audiodata.json')
         .then(response => response.json())
         .then(data => {
             data.map(entry => {
-                if (entry.category.toLowerCase() == category.toLowerCase()) {
+                if (entry.category.toLowerCase() === category.toLowerCase()) {
                     const div = document.createElement('div');
                     div.style.display = 'flex';
                     div.style.flexDirection = 'column';
                     div.style.alignItems = 'center';
                     div.style.marginBottom = '20px';
-                    div.style.overflow = 'hidden';
                     div.classList.add('songCard');
 
                     const songbtn = document.createElement('button');
@@ -28,13 +24,30 @@ document.addEventListener('DOMContentLoaded', function () {
                     img.style.maxWidth = '100%';
                     img.style.height = '100px';
                     img.style.width = '100px';
-                    img.style.maxHeight = '300px';
+
+                    songbtn.addEventListener('click', () => {
+                        if (entry.url) {
+                            window.location.href = entry.url; // url de merda
+                        } else {
+                            alert('URL não disponível para esta música!');
+                        }
+                    });
+
+                    songbtn.addEventListener('contextmenu', (event) => {
+                        event.preventDefault(); 
+                        if (entry.faixa) {
+                            const audio = new Audio(entry.faixa); // mamem o audio
+                            audio.play(); 
+                        } else {
+                            alert('Áudio não disponível para esta música!');
+                        }
+                    });
 
                     div.appendChild(img);
                     div.appendChild(songbtn);
                     songlist.appendChild(div);
                 }
-            })
+            });
         })
         .catch(error => console.log(error));
 });
